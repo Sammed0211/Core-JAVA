bb0c43e8b87c0a10f9eba36f4fc76c9664dcd664
Sammed0211 <108929367+Sammed0211@users.noreply.github.com>
2022-07-15 17:53:11 +0530

    Add files via upload

---
+++ Arithmetic addition.java
@@ -0,0 +1,15 @@
+class Addition {
+public static void main(String[] args)
+    {
+        
+        int num1 = 10, num2 = 20, sum = 0;
+  
+        
+        System.out.println("10");
+        System.out.println("20");
+  
+       
+        sum = 10+20;
+        System.out.println(The sum = 30);
+    }
+}
