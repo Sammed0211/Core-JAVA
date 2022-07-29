0a79b754b210a449950d9b57b69afd23a4c5af79
Sammed0211 <108929367+Sammed0211@users.noreply.github.com>
2022-07-29 15:36:42 +0530

    Add files via upload

---
+++ Variable.java
@@ -0,0 +1,20 @@
+
+public class Variable {
+	public static void main(String [] args)
+	{
+		String name="Paul";
+		float speed=2.66f;
+		byte ram=8;
+		int disk=500;
+		double price=1760.55;
+		char plan ='C';
+	 
+		System.out.println("The Variable name is " +name+ " and the data type used is String" );
+		System.out.println("The Variable Speed is " +speed+" and the data type used is float" );
+		System.out.println("The Variable Ram is " +ram+" and the data type used is byte" );
+		System.out.println("The Variable Disk is " +disk+" and the data type used is int" );
+		System.out.println("The Variable Price is " +price+" and the data type used is double");
+		System.out.println("The Variable Plan is " +plan+" and the data type used is char" );
+	}
+
+}
